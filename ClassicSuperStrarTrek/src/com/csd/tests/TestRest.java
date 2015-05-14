b07486afcc07cf7bbedf51b40d165c8546618d5d
cawelsh1 <C006384@LR9EFBP9.cntr.thrivent.corp>
2015-05-14 11:21:40 -0500

    A4 Rest - fail test

---
+++ ClassicSuperStrarTrek/src/com/csd/tests/TestRest.java
@@ -0,0 +1,14 @@
+package com.csd.tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestRest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
