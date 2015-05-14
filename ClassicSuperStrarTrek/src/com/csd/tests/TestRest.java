8bd832b24f99031d5c1c43926b1064b361c1e598
Veerav Naidu <veeravn@gmail.com>
2015-05-14 14:06:26 -0500

    Add Before Method

--- ClassicSuperStrarTrek/src/com/csd/tests/TestRest.java
+++ ClassicSuperStrarTrek/src/com/csd/tests/TestRest.java
@@ -1,8 +1,13 @@
 
+import org.junit.Before;
 import org.junit.Test;
-
+import static org.junit.Assert.*;
 public class TestRest {
 
+	@Before
+	public void init() {
+		
+	}
 	@Test
 	public void test() {
 		
