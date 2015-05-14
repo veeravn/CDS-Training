ffceb27182aaa90ea45cecff29b7ee6b5d3e5d10
Paul Sasik <psasik@gmail.com>
2015-05-14 11:36:15 -0500

    - removed get/setRaised - got rid of toggle shields - moved things around - initial check-in of shields

--- ClassicSuperStrarTrek/src/com/csd/src/Shields.java
+++ ClassicSuperStrarTrek/src/com/csd/src/Shields.java
@@ -3,16 +3,43 @@ package com.csd.src;
 public class Shields {
 	
 	private boolean raised = false;
+	
+	
+	public static final int DEFAULT_SHIELD_LEVEL = 5000;
+	public static final int MAX_SHIELD_LEVEL = 10000;
+	public static final int MIN_SHIELD_LEVEL = 0;
+	
+	private int shieldEnergyLevel = DEFAULT_SHIELD_LEVEL;
+	
 	public boolean isRaised() {
 		return raised;
 	}
-	public boolean getRaised() {
-		return raised;
+	
+	public void raiseShields() {
+		raised = true;
 	}
-	public void setRaised(boolean raised) {
-		this.raised = raised;
+	
+	public void dropShields() {
+		raised = false;
 	}
-	public void toggleShields() {
-		raised = !raised;
+	
+	
+	public int getShieldEnergyLevel() {
+		return shieldEnergyLevel;
+	}
+	
+	// positive values increase shield levels, negative values reduce them
+	public void changeShieldEnergyLevelBy(int energyUnitDelta) {
+		shieldEnergyLevel = shieldEnergyLevel + energyUnitDelta;
+		
+		if (shieldEnergyLevel < MIN_SHIELD_LEVEL) {
+			shieldEnergyLevel = MIN_SHIELD_LEVEL;
+			return;
+		}
+		
+		if (shieldEnergyLevel > MAX_SHIELD_LEVEL) {
+			shieldEnergyLevel = MAX_SHIELD_LEVEL;
+			return;
+		}
 	}
 }
--- ClassicSuperStrarTrek/src/com/csd/tests/TestShields.java
+++ ClassicSuperStrarTrek/src/com/csd/tests/TestShields.java
@@ -9,27 +9,66 @@ import org.junit.Test;
 
 public class TestShields {
 	
-	Shields shields;
-	@Before
-	public void init() {
-		shields = new Shields();
+	@Test
+	public void shieldsAreNotRaisedByDefault() {
+		Shields s = new Shields();
+		assertFalse(s.isRaised());
 	}
 	
 	@Test
-	public void shieldsAreNotRaisedOutset() {
+	public void shieldDefaultLevelTest() {
+		Shields s = new Shields();
+		assertEquals(s.getShieldEnergyLevel(), Shields.DEFAULT_SHIELD_LEVEL);
+	}
+	
+	@Test
+	public void testDropShields() {
+		Shields shields = new Shields();
+		shields.dropShields();
 		assertFalse(shields.isRaised());
 	}
 	
 	@Test
-	public void toggleLoweredShieldsRaisesShields() {
-		shields.toggleShields();
+	public void testRaiseShields() {
+		Shields shields = new Shields();
+		shields.raiseShields();
 		assertTrue(shields.isRaised());
 	}
 	
-	@Test
-	public void toggleRaisedShieldsLowersShields() {
-		shields.toggleShields();
-		shields.toggleShields();
-		assertFalse(shields.isRaised());
+	@Test 
+	public void testIncreaseShieldEnergyLevel() {
+		Shields s = new Shields();
+		final int energyLevelDelta = 2000;
+		int currentShieldLevel = s.getShieldEnergyLevel();
+		s.changeShieldEnergyLevelBy(energyLevelDelta);
+		assertEquals(s.getShieldEnergyLevel(), currentShieldLevel + energyLevelDelta);
 	}
+	
+	@Test 
+	public void testDecreaseShieldEnergyLevel() {
+		Shields s = new Shields();
+		final int energyLevelDelta = -2000;
+		int currentShieldLevel = s.getShieldEnergyLevel();
+		s.changeShieldEnergyLevelBy(energyLevelDelta);
+		assertEquals(s.getShieldEnergyLevel(), currentShieldLevel + energyLevelDelta);
+	}
+	
+	@Test 
+	public void testIncreaseShieldEnergyLevelBeyondMax() {
+		Shields s = new Shields();
+		final int energyLevelDelta = 5001;
+		s.changeShieldEnergyLevelBy(energyLevelDelta);
+		assertEquals(s.getShieldEnergyLevel(), Shields.MAX_SHIELD_LEVEL);
+	}
+	
+	@Test 
+	public void testDecreaseShieldEnergyLevelBeyondMin() {
+		Shields s = new Shields();
+		assertEquals(s.getShieldEnergyLevel(), Shields.DEFAULT_SHIELD_LEVEL);
+		final int energyLevelDelta = -5002;
+		s.changeShieldEnergyLevelBy(energyLevelDelta);
+		assertEquals(s.getShieldEnergyLevel(), Shields.MIN_SHIELD_LEVEL);
+	}
+	
+	
 }
