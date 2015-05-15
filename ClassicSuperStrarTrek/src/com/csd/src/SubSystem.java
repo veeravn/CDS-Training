33acf41ffb6d7634bf0856f701e4248b227fb265
MARK RAJPAL <marajpal@hotmail.com>
2015-05-15 08:34:26 -0600

    added more functionality to SubSystem

--- ClassicSuperStrarTrek/src/com/csd/src/SubSystem.java
+++ ClassicSuperStrarTrek/src/com/csd/src/SubSystem.java
@@ -1,9 +1,43 @@
+import java.util.HashMap;
 
 public class SubSystem {
 
-    private boolean damaged = false;
+    private double star_dates_to_repair = 0;
+    public static HashMap<subsystem_type, Integer> one_tenth_stardateDamage = new HashMap<subsystem_type, Integer>();
+
+    private subsystem_type type;
+
+    public enum subsystem_type {
+        SHIELDS, PHASERS, WARP_ENGINES, PHOTON_TORPEDOES
+    }
+
+    subsystem_type st;
+
+    public SubSystem(subsystem_type subsystem_type) {
+        type = subsystem_type;
+        one_tenth_stardateDamage.put(st.SHIELDS, 50);
+        one_tenth_stardateDamage.put(st.PHASERS, 30);
+        one_tenth_stardateDamage.put(st.WARP_ENGINES, 20);
+        one_tenth_stardateDamage.put(st.PHOTON_TORPEDOES, 50);
+    }
 
     public boolean isDamaged() {
-        return damaged;
+        if (star_dates_to_repair > 0)
+            return true;
+        else
+            return false;
+    }
+
+    public void applyDamage(int damage) {
+        star_dates_to_repair = star_dates_to_repair + damageCalculation(damage);
+    }
+
+    public subsystem_type getType() {
+        return type;
+    }
+
+    public double damageCalculation(int damage) {
+        return (int) Math.round(damage / one_tenth_stardateDamage.get(type)) * .1;
+
     }
 }
--- ClassicSuperStrarTrek/src/com/csd/tests/TestSubsystemStatus.java
+++ ClassicSuperStrarTrek/src/com/csd/tests/TestSubsystemStatus.java
@@ -1,17 +1,46 @@
 
 
+import org.junit.Before;
 import org.junit.Test;
 
+import static junit.framework.Assert.assertEquals;
 import static junit.framework.Assert.assertFalse;
+import static junit.framework.Assert.assertTrue;
 
 
 public class TestSubsystemStatus {
 
+    SubSystem subsystem;
+
+    @Before
+    public void setup() {
+        subsystem = new SubSystem(SubSystem.subsystem_type.SHIELDS);
+    }
+
     @Test
     public void checkSubsystemStatus() {
-        SubSystem subsystem = new SubSystem();
         assertFalse(subsystem.isDamaged());
 
     }
 
+    @Test
+    public void checkApplyDamage() {
+        subsystem.applyDamage(50);
+        assertTrue(subsystem.isDamaged());
+    }
+
+    @Test
+    public void checkSubsystemType() {
+        assertEquals(SubSystem.subsystem_type.SHIELDS, subsystem.getType());
+    }
+
+    @Test
+    public void checkTypeList() {
+        assertTrue(subsystem.one_tenth_stardateDamage.size() > 0);
+    }
+
+    @Test
+    public void checkDamageCalculation() {
+        assertEquals(.1, subsystem.damageCalculation(59));
+    }
 }
